0e6c53e1af26958aab45677cbeed0067d989ab6b
namazso <admin@namazso.eu>
2023-06-11 11:39:02 +0200

    Add license

--- PawnIO/natives_impl_windows.cpp
+++ PawnIO/natives_impl_windows.cpp
@@ -1,3 +1,49 @@
+// PawnIO - Input-output driver
+// Copyright (C) 2023  namazso <admin@namazso.eu>
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+// 
+// Linking PawnIO statically or dynamically with other modules is making a
+// combined work based on PawnIO. Thus, the terms and conditions of the GNU
+// General Public License cover the whole combination.
+// 
+// In addition, as a special exception, the copyright holders of PawnIO give
+// you permission to combine PawnIO program with free software programs or
+// libraries that are released under the GNU LGPL and with independent modules
+// that communicate with PawnIO solely through the device IO control
+// interface. You may copy and distribute such a system following the terms of
+// the GNU GPL for PawnIO and the licenses of the other code concerned,
+// provided that you include the source code of that other code when and as
+// the GNU GPL requires distribution of source code.
+// 
+// Note that this exception does not include programs that communicate with
+// PawnIO over the Pawn interface. This means that all modules loaded into
+// PawnIO must be compatible with this licence, including the earlier
+// exception clause. We recommend using the GNU Lesser General Public License
+// version 2.1 to fulfill this requirement.
+// 
+// For alternative licensing options, please contact the copyright holder at
+// admin@namazso.eu.
+// 
+// Note that people who make modified versions of PawnIO are not obligated to
+// grant this special exception for their modified versions; it is their
+// choice whether to do so. The GNU General Public License gives permission
+// to release a modified version without this exception; this exception also
+// makes it possible to release a modified version which carries forward this
+// exception.
+
 #include <intrin.h>
 #include <ntddk.h>
 #include "natives_impl.h"

44fe52fc80eb7c3af1d69d0db0315118651ce1c5
namazso <admin@namazso.eu>
2025-09-20 12:32:49 +0200

    Add version info

---
+++ PawnIO/version.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define PAWNIO_NAME "PawnIO"
+#define PAWNIO_NAME_FULL "PawnIO Official Build"
+#define PAWNIO_AUTHOR "namazso"
+#define PAWNIO_MAJOR 2
+#define PAWNIO_MINOR 1
+#define PAWNIO_PATCH 0
+#define PAWNIO_VERSION_STRING "2.1.0"
+#define PAWNIO_VERSION_STRING4 "2.1.0.0"

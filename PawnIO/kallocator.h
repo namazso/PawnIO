ec1abb41ffbdacd1c009d2e40216a3a09fca6bb2
namazso <admin@namazso.eu>
2025-09-21 10:23:19 +0200

    Add kallocator / klist

---
+++ PawnIO/kallocator.h
@@ -0,0 +1,121 @@
+// PawnIO - Input-output driver
+// Copyright (C) 2023  namazso <admin@namazso.eu>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+// Linking PawnIO statically or dynamically with other modules is making a
+// combined work based on PawnIO. Thus, the terms and conditions of the GNU
+// General Public License cover the whole combination.
+//
+// In addition, as a special exception, the copyright holders of PawnIO give
+// you permission to combine PawnIO program with free software programs or
+// libraries that are released under the GNU LGPL and with independent modules
+// that communicate with PawnIO solely through the device IO control
+// interface. You may copy and distribute such a system following the terms of
+// the GNU GPL for PawnIO and the licenses of the other code concerned,
+// provided that you include the source code of that other code when and as
+// the GNU GPL requires distribution of source code.
+//
+// Note that this exception does not include programs that communicate with
+// PawnIO over the Pawn interface. This means that all modules loaded into
+// PawnIO must be compatible with this licence, including the earlier
+// exception clause. We recommend using the GNU Lesser General Public License
+// version 2.1 to fulfill this requirement.
+//
+// For alternative licensing options, please contact the copyright holder at
+// admin@namazso.eu.
+//
+// Note that people who make modified versions of PawnIO are not obligated to
+// grant this special exception for their modified versions; it is their
+// choice whether to do so. The GNU General Public License gives permission
+// to release a modified version without this exception; this exception also
+// makes it possible to release a modified version which carries forward this
+// exception.
+
+#pragma once
+
+template <typename T, POOL_TYPE PoolType = NonPagedPoolNx>
+class kallocator {
+  static constexpr ULONG k_tag = 'olAk';
+
+public:
+  // Standard allocator typedefs
+  using value_type = T;
+  using pointer = T*;
+  using const_pointer = const T*;
+  using reference = T&;
+  using const_reference = const T&;
+  using size_type = size_t;
+  using difference_type = ptrdiff_t;
+  using propagate_on_container_move_assignment = std::true_type;
+  using is_always_equal = std::true_type;
+
+  constexpr kallocator() noexcept = default;
+  constexpr kallocator(const kallocator&) noexcept = default;
+  constexpr kallocator(kallocator&&) noexcept = default;
+  constexpr kallocator& operator=(const kallocator&) noexcept = default;
+  constexpr kallocator& operator=(kallocator&&) noexcept = default;
+  ~kallocator() = default;
+
+  // Rebind for allocating other types
+  template <typename U>
+  struct rebind {
+    using other = kallocator<U, PoolType>;
+  };
+
+  // Copy constructor
+  template <typename U>
+  kallocator(const kallocator<U, PoolType>&) noexcept {}
+
+  // Required allocator functions
+  pointer allocate(size_type n) {
+    // Allocate n objects of type T from kernel pool
+    const size_t bytes = n * sizeof(T);
+
+    void* p = ExAllocatePoolZero(PoolType, bytes, k_tag);
+    /*if (!p) {
+      KeBugCheckEx(DRIVER_OVERRAN_STACK_BUFFER, 0, 0, 0, 0);
+    }*/
+    return static_cast<pointer>(p);
+  }
+
+  void deallocate(pointer p, size_type) noexcept {
+    if (p) {
+      ExFreePoolWithTag(p, k_tag);
+    }
+  }
+
+  // Construction/destruction of objects
+  template <typename U, typename... Args>
+  void construct(U* p, Args&&... args) {
+    ::new (static_cast<void*>(p)) U(std::forward<Args>(args)...);
+  }
+
+  template <typename U>
+  void destroy(U* p) {
+    p->~U();
+  }
+};
+
+// Equality comparison operators
+template <typename T1, typename T2, POOL_TYPE PoolType>
+bool operator==(const kallocator<T1, PoolType>&, const kallocator<T2, PoolType>&) noexcept {
+  return true;
+}
+
+template <typename T1, typename T2, POOL_TYPE PoolType>
+bool operator!=(const kallocator<T1, PoolType>&, const kallocator<T2, PoolType>&) noexcept {
+  return false;
+}
---
+++ PawnIO/klist.h
@@ -0,0 +1,375 @@
+// PawnIO - Input-output driver
+// Copyright (C) 2023  namazso <admin@namazso.eu>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+// Linking PawnIO statically or dynamically with other modules is making a
+// combined work based on PawnIO. Thus, the terms and conditions of the GNU
+// General Public License cover the whole combination.
+//
+// In addition, as a special exception, the copyright holders of PawnIO give
+// you permission to combine PawnIO program with free software programs or
+// libraries that are released under the GNU LGPL and with independent modules
+// that communicate with PawnIO solely through the device IO control
+// interface. You may copy and distribute such a system following the terms of
+// the GNU GPL for PawnIO and the licenses of the other code concerned,
+// provided that you include the source code of that other code when and as
+// the GNU GPL requires distribution of source code.
+//
+// Note that this exception does not include programs that communicate with
+// PawnIO over the Pawn interface. This means that all modules loaded into
+// PawnIO must be compatible with this licence, including the earlier
+// exception clause. We recommend using the GNU Lesser General Public License
+// version 2.1 to fulfill this requirement.
+//
+// For alternative licensing options, please contact the copyright holder at
+// admin@namazso.eu.
+//
+// Note that people who make modified versions of PawnIO are not obligated to
+// grant this special exception for their modified versions; it is their
+// choice whether to do so. The GNU General Public License gives permission
+// to release a modified version without this exception; this exception also
+// makes it possible to release a modified version which carries forward this
+// exception.
+
+#pragma once
+
+#include "kallocator.h"
+
+template <typename T>
+struct klist_node {
+  LIST_ENTRY entry;
+  T value;
+};
+
+template <typename T>
+klist_node<T>* get_node_from_entry(PLIST_ENTRY entry) {
+  return CONTAINING_RECORD(entry, klist_node<T>, entry);
+}
+
+template <typename List>
+class klist_iterator {
+public:
+  using iterator_category = std::bidirectional_iterator_tag;
+  using value_type = typename List::value_type;
+  using difference_type = typename List::difference_type;
+  using pointer = typename List::pointer;
+  using reference = typename List::reference;
+
+  friend List;
+
+  template <typename OtherList>
+  friend class klist_iterator;
+private:
+  List* _list;
+  PLIST_ENTRY _entry;
+
+  klist_iterator() : _list(nullptr), _entry(nullptr) {}
+  klist_iterator(List* list, PLIST_ENTRY entry) : _list(list), _entry(entry) {}
+
+public:
+  operator klist_iterator<const List>() const {
+    return klist_iterator<const List>(_list, _entry);
+  }
+
+  [[nodiscard]] PLIST_ENTRY as_entry() const { return _entry; }
+
+  reference operator*() const {
+    if (_entry == _list->end().as_entry())
+      __fastfail(FAST_FAIL_RANGE_CHECK_FAILURE);
+    
+    return get_node_from_entry<value_type>(_entry)->value;
+  }
+
+  pointer operator->() const {
+    if (_entry == _list->end().as_entry())
+      __fastfail(FAST_FAIL_RANGE_CHECK_FAILURE);
+
+    return &get_node_from_entry<value_type>(_entry)->value;
+  }
+
+  klist_iterator& operator++() {
+    _entry = _entry->Flink;
+    return *this;
+  }
+
+  klist_iterator operator++(int) {
+    klist_iterator tmp = *this;
+    _entry = _entry->Flink;
+    return tmp;
+  }
+
+  klist_iterator& operator--() {
+    _entry = _entry->Blink;
+    return *this;
+  }
+
+  klist_iterator operator--(int) {
+    klist_iterator tmp = *this;
+    _entry = _entry->Blink;
+    return tmp;
+  }
+
+  [[nodiscard]] bool operator==(const klist_iterator& other) const {
+    return _entry == other._entry;
+  }
+
+  [[nodiscard]] bool operator!=(const klist_iterator& other) const {
+    return _entry != other._entry;
+  }
+};
+
+template <class T, class Allocator = kallocator<klist_node<T>>>
+class klist {
+public:
+  using value_type = T;
+  using allocator_type = Allocator;
+  using size_type = size_t;
+  using difference_type = ptrdiff_t;
+  using reference = value_type&;
+  using const_reference = const value_type&;
+  using pointer = typename std::allocator_traits<Allocator>::pointer;
+  using const_pointer = typename std::allocator_traits<Allocator>::const_pointer;
+  using iterator = klist_iterator<klist<T, Allocator>>;
+  using const_iterator = klist_iterator<const klist<T, Allocator>>;
+  using reverse_iterator = std::reverse_iterator<iterator>;
+  using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+private:
+  Allocator _alloc{};
+  LIST_ENTRY _head{};
+
+public:
+  constexpr klist() noexcept {
+    _head.Flink = &_head;
+    _head.Blink = &_head;
+  }
+
+  klist(const klist& other) = delete;
+
+  klist(klist&& other) noexcept : _alloc(std::move(other._alloc)) {
+    InitializeListHead(&_head);
+    if (!other.empty()) {
+      _head = other._head;
+      _head.Flink->Blink = &_head;
+      _head.Blink->Flink = &_head;
+      InitializeListHead(&other._head);
+    }
+  }
+
+  klist& operator=(const klist& other) = delete;
+
+  klist& operator=(klist&& other) noexcept {
+    if (this != &other) {
+      clear();
+      if constexpr (std::allocator_traits<Allocator>::propagate_on_container_move_assignment::value) {
+        _alloc = std::move(other._alloc);
+      }
+      if (!other.empty()) {
+        _head = other._head;
+        _head.Flink->Blink = &_head;
+        _head.Blink->Flink = &_head;
+        InitializeListHead(&other._head);
+      }
+    }
+    return *this;
+  }
+
+  ~klist() {
+    clear();
+  }
+
+  [[nodiscard]] iterator iter_from_entry(PLIST_ENTRY entry) { return iterator(this, entry); }
+  [[nodiscard]] const_iterator iter_from_entry(PLIST_ENTRY entry) const { return const_iterator(this, entry); }
+  [[nodiscard]] const_iterator citer_from_entry(PLIST_ENTRY entry) const { return const_iterator(this, entry); }
+  
+  [[nodiscard]] iterator begin() { return iterator(this, _head.Flink); }
+  [[nodiscard]] const_iterator begin() const { return const_iterator(this, _head.Flink); }
+  [[nodiscard]] const_iterator cbegin() const { return const_iterator(this, _head.Flink); }
+  
+  [[nodiscard]] iterator end() { return iterator(this, &_head); }
+  [[nodiscard]] const_iterator end() const { return const_iterator(this, &_head); }
+  [[nodiscard]] const_iterator cend() const { return const_iterator(this, &_head); }
+  
+  [[nodiscard]] iterator rbegin() { return reverse_iterator(end()); }
+  [[nodiscard]] const_iterator rbegin() const { return const_reverse_iterator(end()); }
+  [[nodiscard]] const_iterator crbegin() const { return const_reverse_iterator(cend()); }
+  
+  [[nodiscard]] iterator rend() { return reverse_iterator(begin()); }
+  [[nodiscard]] const_iterator rend() const { return const_reverse_iterator(begin()); }
+  [[nodiscard]] const_iterator crend() const { return const_reverse_iterator(cbegin()); }
+
+  [[nodiscard]] bool empty() const { return IsListEmpty(&_head) != FALSE; }
+
+  void clear();
+  iterator insert(const_iterator pos, const T& value);
+  iterator insert(const_iterator pos, T&& value);
+  //iterator insert(const_iterator pos, size_type count, const T& value);
+  //template <class InputIt>
+  //iterator insert(const_iterator pos, InputIt first, InputIt last);
+  //iterator insert(const_iterator pos, std::initializer_list<T> ilist);
+  template <class... Args>
+  iterator emplace(const_iterator pos, Args&&... args);
+  iterator erase(iterator pos);
+  iterator erase(const_iterator pos);
+  iterator erase(iterator first, iterator last);
+  iterator erase(const_iterator first, const_iterator last);
+  void push_back(const T& value);
+  void push_back(T&& value);
+  //template <class... Args>
+  //reference emplace_back(Args&&... args);
+  void push_front(const T& value);
+  void push_front(T&& value);
+  template <class... Args>
+  void emplace_front(Args&&... args);
+  //template <class... Args>
+  //reference emplace_front(Args&&... args);
+  //void resize(size_type count);
+  //void resize(size_type count, const value_type& value);
+  void swap(klist& other) noexcept;
+};
+
+template <class T, class Allocator>
+void klist<T, Allocator>::clear() {
+  while (!empty()) {
+    erase(begin());
+  }
+}
+
+template <class T, class Allocator>
+typename klist<T, Allocator>::iterator klist<T, Allocator>::insert(const_iterator pos, const T& value) {
+  auto* node = std::allocator_traits<Allocator>::allocate(_alloc, 1);
+  if (!node)
+    return end();
+  std::allocator_traits<Allocator>::construct(_alloc, &node->value, value);
+
+  InsertHeadList(pos._entry->Blink, &node->entry);
+  return iterator(this, &node->entry);
+}
+
+template <class T, class Allocator>
+typename klist<T, Allocator>::iterator klist<T, Allocator>::insert(const_iterator pos, T&& value) {
+  auto* node = std::allocator_traits<Allocator>::allocate(_alloc, 1);
+  if (!node)
+    return end();
+  std::allocator_traits<Allocator>::construct(_alloc, &node->value, std::move(value));
+
+  InsertHeadList(pos._entry->Blink, &node->entry);
+  return iterator(this, &node->entry);
+}
+
+template <class T, class Allocator>
+template <class... Args>
+typename klist<T, Allocator>::iterator klist<T, Allocator>::emplace(const_iterator pos, Args&&... args) {
+  auto* node = std::allocator_traits<Allocator>::allocate(_alloc, 1);
+  if (!node)
+    return end();
+  std::allocator_traits<Allocator>::construct(_alloc, &node->value, std::forward<Args>(args)...);
+
+  InsertHeadList(pos._entry->Blink, &node->entry);
+  return iterator(this, &node->entry);
+}
+
+template <class T, class Allocator>
+typename klist<T, Allocator>::iterator klist<T, Allocator>::erase(iterator pos) {
+  PLIST_ENTRY next = pos._entry->Flink;
+  RemoveEntryList(pos._entry);
+
+  auto* node = get_node_from_entry<T>(pos._entry);
+  std::allocator_traits<Allocator>::destroy(_alloc, &node->value);
+  std::allocator_traits<Allocator>::deallocate(_alloc, node, 1);
+
+  return iterator(this, next);
+}
+
+template <class T, class Allocator>
+typename klist<T, Allocator>::iterator klist<T, Allocator>::erase(const_iterator pos) {
+  return erase(iterator(this, pos._entry));
+}
+
+template <class T, class Allocator>
+typename klist<T, Allocator>::iterator klist<T, Allocator>::erase(iterator first, iterator last) {
+  while (first != last) {
+    first = erase(first);
+  }
+  return last;
+}
+
+template <class T, class Allocator>
+typename klist<T, Allocator>::iterator klist<T, Allocator>::erase(const_iterator first, const_iterator last) {
+  return erase(iterator(this, first._entry), iterator(this, last._entry));
+}
+
+template <class T, class Allocator>
+void klist<T, Allocator>::push_back(const T& value) {
+  insert(end(), value);
+}
+
+template <class T, class Allocator>
+void klist<T, Allocator>::push_back(T&& value) {
+  insert(end(), std::move(value));
+}
+
+template <class T, class Allocator>
+void klist<T, Allocator>::push_front(const T& value) {
+  insert(begin(), value);
+}
+
+template <class T, class Allocator>
+void klist<T, Allocator>::push_front(T&& value) {
+  insert(begin(), std::move(value));
+}
+
+template <class T, class Allocator>
+template <class... Args>
+void klist<T, Allocator>::emplace_front(Args&&... args) {
+  emplace(begin(), std::forward<Args>(args)...);
+}
+
+template <class T, class Allocator>
+void klist<T, Allocator>::swap(klist& other) noexcept {
+  if (this == &other)
+    return;
+
+  if constexpr (std::allocator_traits<Allocator>::propagate_on_container_swap::value) {
+    std::swap(_alloc, other._alloc);
+  }
+
+  // Swap only if both lists are non-empty
+  if (!empty() && !other.empty()) {
+    std::swap(_head, other._head);
+
+    // Fix the pointers
+    _head.Flink->Blink = &_head;
+    _head.Blink->Flink = &_head;
+    other._head.Flink->Blink = &other._head;
+    other._head.Blink->Flink = &other._head;
+  }
+  // If this is empty but other is not
+  else if (empty() && !other.empty()) {
+    _head = other._head;
+    _head.Flink->Blink = &_head;
+    _head.Blink->Flink = &_head;
+    InitializeListHead(&other._head);
+  }
+  // If this is not empty but other is
+  else if (!empty() && other.empty()) {
+    other._head = _head;
+    other._head.Flink->Blink = &other._head;
+    other._head.Blink->Flink = &other._head;
+    InitializeListHead(&_head);
+  }
+  // If both are empty, nothing to do
+}
